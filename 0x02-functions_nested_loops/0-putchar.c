e7df4b53eff11d11e8dbfea7e9573922c6832cb4
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-03-15 14:46:09 +0300

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include "main.h">
+#include <unistd.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0..
- */
+*putchar to print the characters given
+* main putchar
+*char c the charcters given
+* Return: Always 0 (Success)
+*/
+int putchar(char c)
+{
+	write(1, &c, 8);
+        return(0);
+}
 int main(void)
 {
-	puts("_putchar\n");
-
+	putchar("_putchar\n");
 	return (0);
 }
